38a13c12bc5c342a93eafe1ff588620cdbeda3d7
ParampreetR <raja.rai6789@gmail.com>
2022-03-08 15:49:44 +0530

    Basic Hello World with `Fastify`

---
+++ index.js
@@ -0,0 +1,33 @@
+const Fastify = require("fastify"); // Backend Framework
+let app = Fastify();
+
+/*
+ * Hosting providers wants all Web Applications to bind
+ * on PORT specified by them thorugh environment variable `PORT`
+ */
+const PORT = process.env.PORT | 8080;
+
+/*
+ * General Structure of Request Handler
+ *  app.[get|post|puts](<path>, (<Request Object>, <Response Object>) => {
+ *    // Process Request and Return response by
+ *    // <Response Object>.send(<Data to return>)
+ *  });
+ */
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+/*
+ * Bind and Listen on specified PORT and address
+ * Note: Some Hosting providers wants developers to host
+ *  Web Application at all interface, example `0.0.0.0`.
+ */
+app.listen(PORT, "localhost", (err, addr) => {
+  if (err) {
+    console.err(err);
+    process.exit(1);
+  }
+
+  console.info("Server live at " + addr);
+});
